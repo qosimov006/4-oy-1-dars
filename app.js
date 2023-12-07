3d81de789d7c60355c19a43e19ccc021cf417964
qsosimov006 <mirjalolq274@gmail.com>
2023-12-07 19:55:25 +0500

    Javascrip dastlabki cod

---
+++ app.js
@@ -0,0 +1,66 @@
+//1-misol
+
+/*
+
+let number1 = +prompt("Ixtioriy soni kiriting");
+let number2 = +prompt("Ixtioriy soni kiriting");
+let number3 = +prompt("Ixtioriy soni kiriting");
+
+alert(number1+number2+number3);
+*/
+
+
+
+// 2-misol
+
+/*
+let num1 = prompt("Soni kiriting");
+let num2 = prompt("Soni kiriting");
+
+alert(num1*2+num2*2); */
+
+// 3-misol 
+
+ /*
+    let a =  +prompt("To'g'ri tomon uzunligini kiriting:");
+    let b =  +prompt("Ikkinchi to'g'ri tomon uzunligini kiriting:");
+    let c =  +prompt("Uchinchi to'g'ri tomon uzunligini kiriting:");
+    let d =  +prompt("To'rtinchi to'g'ri tomon uzunligini kiriting:");
+
+    alert(a+b+c+d) 
+    */
+  
+
+
+// 4- misol
+/*
+
+var tomon = prompt("Kubning tomonini kiriting:");
+var hajm = Math.pow(tomon, 3);
+var tolaSirt = 6 * Math.pow(tomon, 2);
+alert("Kubning to'la sirti: " + tolaSirt);
+alert("Kubning hajmi: " + hajm);
+*/
+
+
+
+
+// 5-misol
+/*
+
+var diametr = prompt("Doira diametrini kiriting:");
+var radius = diametr / 2;
+var yuza = Math.PI * Math.pow(radius, 2);
+alert("Doira yuzi: " + yuza);
+*/
+
+
+
+
+
+
+
+
+
+
+
